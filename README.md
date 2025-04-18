35b6b296f5907073ecb7a27fb27ab0490a4f1003
Niez <131792942+Nieznany237@users.noreply.github.com>
2025-04-18 23:41:20 +0200

    Commit
    #1

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Vocabulary-Practice-App
-A simple and effective app for practicing vocabulary in two languages using custom word lists.
+## Version: 1.2.0 [18.04.2025]
+
+# Vocabulary Practice App
+
+**Vocabulary Practice App** is a simple and effective tool for practicing vocabulary between any two languages using your own word lists.
+
+## How to Use It?
+
+1. **Prepare a `.txt` file with your vocabulary.**  
+   The file should start with a **header line** indicating the two languages, followed by word pairs separated by a dash `-`.
+
+   ### 📄 Structure:
+   - The **header** is required and helps the app identify translation directions.
+   - Word pairs must follow this format: `Language1Word - Language2Word`
+
+   ### Example:
+   ```text
+   English - Polish
+   Astronaut - Astronauta
+   Barrister - Adwokat
+   Chief executive officer - Dyrektor generalny
+   ```
+
+2. **Launch the app** and load your vocabulary file using the menu.
+
+3. **Choose a mode**:
+- Language 1 → Language 2
+- Language 2 → Language 1
+- Mixed (default)
+
+4. **Start practicing!**  
+Type in the translation, check your answer, get hints if needed, and track your accuracy.
+
+---
+
+## Required Libraries
+
+| Library                                                         | Purpose                    | Installation                  |
+|-----------------------------------------------------------------|----------------------------|-------------------------------|
+| [CustomTkinter](https://github.com/TomSchimansky/CustomTkinter) | Modern UI components       | `pip install customtkinter`   |
+| [CTkMenuBar](https://github.com/Akascape/CTkMenuBar)            | Modern MenuBar             | `pip install CTkMenuBar`      |
+| [Pillow](https://pypi.org/project/pillow/)                      | Image handling             | `pip install pillow`          |
+
+### To install all the required packages, use the following command in a terminal (make sure you have Python and pip installed):
+
+```bash
+pip install -r requirements.txt
+```
+
+
+
+## Examples
+
+Here are examples of how the application looks:
+
+![screenshot](examples/1.png) 
+![screenshot](examples/2.png)
+![screenshot](examples/3.png)
+![screenshot](examples/4.png)
---
+++ translation.py
@@ -0,0 +1,156 @@
+# This file contains translations for the application in Polish and English.
+
+# To use Polish, set "LANGUAGE": "pl" in the config file.
+# To use English, set "LANGUAGE": "en" in the config file.
+
+# Structure version 1.0.0
+TRANSLATIONS_PL = {
+    "menubar": {
+        "file": {
+            "file": "Plik",
+            "load_file": "Wczytaj plik",
+            "exit": "Zakończ",
+        },
+        "appearance": {
+            "appearance": "Wygląd",
+            "dark_mode": "Dark Mode",
+            "light_mode": "Light Mode",
+            "zoom_in": "Powiększ UI",
+            "zoom_out": "Pomniejsz UI",
+        },
+        "about": {
+            "about": "O aplikacji",
+            "about_this_app": "O tej aplikacji",
+            "get_program_path_debug": "Get program path [DEBUG]",
+            "get_cache_info": "Print cache info [CLI - DEBUG]",
+        }
+    },
+    
+    "main_window": {
+        "question_label": {
+            "default": "Załaduj plik, aby rozpocząć",
+            "File_error": "Plik jest pusty lub niewłaściwy!",
+            "TranslateIt": "Podaj tłumaczenie słowa:",
+            "No_words": "Brak dostępnych słówek do wyświetlenia!",
+            "Not_loaded": "Brak załadowanych słówek!"
+            },
+        "line_info_label": "Losowo wybrana linia pliku:",
+        "entry_placeholder": "Wpisz tutaj tłumaczenie",
+        "buttons": {
+            "hint_button": "Ujawnij pierwsze litery",
+            "check_button": "Sprawdź",
+            "skip_button": "Pomiń/Przejdź dalej",
+            "file_button": "Załaduj plik .txt",
+            "clear_button": "Wyczyść listę blokady"
+        },
+        "mode_label": "Wybierz tryb tłumaczenia:",
+        "Radiobuttons": {
+        "left": "Lewy",
+        "right": "Prawy",
+        "both": "Mieszane",
+        },
+        "checkbox": {
+            "block_list": "Blokuj powtarzające się pytania",
+        },
+        "result_label": {
+            "default": "Tu będzie wynik",
+            "percent": "Procent poprawności:",
+            "correct": "Poprawna odpowiedź:",
+            "score": "Wynik:",
+            "percentage": "Procent poprawnych odpowiedzi:",
+            "No_words": "Brak słowa do sprawdzenia!",
+            "hint_text": "Podpowiedź:"
+        },
+
+    },
+
+    "about_window": {
+        "about_window_title": "O aplikacji",
+        
+        "program_info_description": {
+            "program_name": "Nazwa programu",
+            "version": "Wersja",
+            "author": "Autor",
+            "release_date": "Data wydania",
+            "first_release": "Pierwsze wydanie",
+            "licence": "Licencja",
+            "description": "Opis"
+        },
+    }
+}
+
+# The English translation is the default one, so it doesn't need to be defined in the same way as the Polish one.
+TRANSLATIONS_EN = {
+    "menubar": {
+        "file": {
+            "file": "File",
+            "load_file": "Load File",
+            "exit": "Exit",
+        },
+        "appearance": {
+            "appearance": "Appearance",
+            "dark_mode": "Dark Mode",
+            "light_mode": "Light Mode",
+            "zoom_in": "Zoom In",
+            "zoom_out": "Zoom Out",
+        },
+        "about": {
+            "about": "About",
+            "about_this_app": "About this app",
+            "get_program_path_debug": "Get program path [DEBUG]",
+            "get_cache_info": "Print cache info [CLI - DEBUG]",
+        }
+    },
+    
+    "main_window": {
+        "question_label": {
+            "default": "Load the file to start",
+            "File_error": "The file is empty or invalid!",
+            "TranslateIt": "Provide the translation of this word:",
+            "No_words": "No available words to display!",
+            "Not_loaded": "No words loaded!"
+            },
+        "line_info_label": "Randomly selected line from the file:",
+        "entry_placeholder": "Enter the translation here",
+        "buttons": {
+            "hint_button": "Reveal first letters",
+            "check_button": "Check",
+            "skip_button": "Skip/Next",
+            "file_button": "Load .txt file",
+            "clear_button": "Clear block list"
+        },
+        "mode_label": "Choose translation mode:",
+        "Radiobuttons": {
+        "left": "Left",
+        "right": "Right",
+        "both": "Mixed",
+        },
+        "checkbox": {
+            "block_list": "Block repeated questions",
+        },
+        "result_label": {
+            "default": "Result will appear here",
+            "percent": "Accuracy percentage:",
+            "correct": "Correct answer:",
+            "score": "Score:",
+            "percentage": "Percentage of correct answers:",
+            "No_words": "No word to check!",
+            "hint_text": "Hint:"
+        },
+
+    },
+
+    "about_window": {
+        "about_window_title": "About the application",
+        
+        "program_info_description": {
+            "program_name": "Program name",
+            "version": "Version",
+            "author": "Author",
+            "release_date": "Release date",
+            "first_release": "First release",
+            "licence": "License",
+            "description": "Description"
+        },
+    }
+}
